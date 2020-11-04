959c9e2ee56e5aeecdfaa3c7c3077d50ab20d8b4
Vehicle Researcher <>
2020-11-05 07:40:15 +0900

    Update dashcam.h

--- selfdrive/ui/dashcam.h
+++ selfdrive/ui/dashcam.h
@@ -269,8 +269,10 @@ static void screen_draw_button(UIState *s, int touch_x, int touch_y) {
     int btn_h = 150;
     int btn_x = 1920 - btn_w;
     int btn_y = 1080 - btn_h;
+    int btn_xc = btn_x + (btn_w/2)
+    int btn_yc = btn_y + (btn_h/2)
     nvgBeginPath(s->vg);
-      nvgRoundedRect(s->vg, btn_x-110, btn_y-45, btn_w, btn_h, 100);
+      nvgRoundedRect(s->vg, btn_x, btn_y, btn_w, btn_h, 100);
       nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
       nvgStrokeWidth(s->vg, 6);
       nvgStroke(s->vg);
@@ -286,7 +288,8 @@ static void screen_draw_button(UIState *s, int touch_x, int touch_y) {
       else {
         nvgFillColor(s->vg, nvgRGBA(255, 255, 255, 200));
       }
-      nvgText(s->vg,btn_x-38,btn_y+50,"REC",NULL);
+      nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
+      nvgText(s->vg,btn_xc,btn_yc,"REC",NULL);
   }
 
   if (captureState == CAPTURE_STATE_CAPTURING) {
