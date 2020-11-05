35a3a80e49d40838316b85cdc03af677418326fc
Vehicle Researcher <>
2020-11-06 00:23:33 +0900

    Update latcontrol.h

--- selfdrive/ui/latcontrol.h
+++ selfdrive/ui/latcontrol.h
@@ -35,25 +35,23 @@ static void draw_control_buttons(UIState *s, int touch_x, int touch_y) {
     int btn_xc2 = btn_x2 + (btn_w/2);
     int btn_yc = btn_y + (btn_h/2);
     nvgBeginPath(s->vg);
+    nvgRoundedRect(s->vg, btn_x1, btn_y, btn_w, btn_h, 100);
+    nvgRoundedRect(s->vg, btn_x2, btn_y, btn_w, btn_h, 100);
     nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
     nvgStrokeWidth(s->vg, 6);
     nvgStroke(s->vg);
-    nvgRoundedRect(s->vg, btn_x1, btn_y, btn_w, btn_h, 100);
-    nvgRoundedRect(s->vg, btn_x2, btn_y, btn_w, btn_h, 100);
     
     nvgFontSize(s->vg, 50);
+    
     if (s->lateral_control == 0) {
-      nvgFill(s->vg);
       nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
       nvgText(s->vg,btn_xc1,btn_yc,"INDI",NULL);
       nvgText(s->vg,btn_xc2,btn_yc,"LQR",NULL);
     } else if (s->lateral_control == 1) {
-      nvgFill(s->vg);
       nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
       nvgText(s->vg,btn_xc1,btn_yc,"PID",NULL);
       nvgText(s->vg,btn_xc2,btn_yc,"LQR",NULL);
     } else if (s->lateral_control == 2) {
-      nvgFill(s->vg);
       nvgFillColor(s->vg, nvgRGBA(255,255,255,200));
       nvgText(s->vg,btn_xc1,btn_yc,"INDI",NULL);
       nvgText(s->vg,btn_xc2,btn_yc,"PID",NULL);
