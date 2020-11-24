8138ecec390cb3a7c8eab8c0351498250712c81b
yunbong2 <49453243+yunbong2@users.noreply.github.com>
2020-11-24 21:16:06 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -93,8 +93,8 @@ class PathPlanner():
     self.new_steer_rate_cost = CP.steerRateCost
     #self.steer_rate_cost_range = [CP.steerRateCost, 0.1]
 
-    self.steer_actuator_delay_range = [0.1, CP.steerActuatorDelay]
-    self.steer_actuator_delay_vel = [3, 13]
+    self.steer_actuator_delay_range = [0.3, CP.steerActuatorDelay, CP.steerActuatorDelay, 0.1]
+    self.steer_actuator_delay_vel = [3, 9, 18, 19]
     self.new_steer_actuator_delay = CP.steerActuatorDelay
 
     self.angle_offset_select = int(Params().get('OpkrAngleOffsetSelect'))
