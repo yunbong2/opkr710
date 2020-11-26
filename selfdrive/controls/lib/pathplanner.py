1623c7555b82b7531decf764b1dc8463ff78fc81
yunbong2 <49453243+yunbong2@users.noreply.github.com>
2020-11-26 11:19:25 +0900

    Update pathplanner.py

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -86,8 +86,8 @@ class PathPlanner():
     self.lane_change_adjust_vel = [8.3, 16, 22, 30]
     self.lane_change_adjust_new = 0.0
 
-    self.angle_differ_range = [0, 5, 9]
-    self.steerRatio_range = [CP.steerRatio, CP.steerRatio, 18]
+    self.angle_differ_range = [0, 45]
+    self.steerRatio_range = [CP.steerRatio, 18]
     self.new_steerRatio = CP.steerRatio
 
     self.new_steer_rate_cost = CP.steerRateCost
